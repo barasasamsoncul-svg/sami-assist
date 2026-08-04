ecf6a145f164f58e1dbea434f24fe85542e86578
Samson Barasa <barasasamsoncul@gmail.com>
2026-08-04 20:22:09 +0300

    fix invoicing schema UTF8 encoding

--- lib/apps/invoicing/schema.sql
+++ lib/apps/invoicing/schema.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE IF NOT EXISTS public.customers (
+CREATE TABLE IF NOT EXISTS public.customers (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     company_name VARCHAR(255) NOT NULL,
     contact_name VARCHAR(255),
